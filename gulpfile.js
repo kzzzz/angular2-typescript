9635fc489942e1b654a2ac4d7df6c7d6eafcc6c0
KZ <zhiping72@gmail.com>
2016-03-29 23:53:18 +0200

    Gulp file init

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,14 @@
-/**
- * Created by Zhiping on 28-3-2016.
- */
+var gulp = require('gulp');
+
+gulp.task('default', ['script', 'style'], function () {
+    console.log('gulp started');
+});
+
+
+gulp.task('script', function () {
+
+});
+
+gulp.task('style', function () {
+
+});
